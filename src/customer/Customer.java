dc2b6be0b9d9612978f7c00d238f45e32afe933b
zactivities <baxz@seattleu.edu>
2020-03-02 14:52:35 -0800

    Created Customer Status ENUM and Customer variables

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,5 +1,14 @@
 package customer;
+import enums.CustomerStatus;
 
 public class Customer {
 
+	
+	private String firstName;
+	private String lastName;
+	private CustomerStatus status;
+	private String phone;
+	
 }
+
+
